2a2ec1f1c87f92ef19cf0ea0bba89ad2c5a246e4
Faycal-Bezleghdir <b-faycal@live.fr>
2023-07-06 13:30:49 -0400

    ajouter Nav et la page Home

---
+++ src/components/Home.js
@@ -0,0 +1,23 @@
+function Home() {
+    return (
+      <div className="container">
+        <div className="home">
+          <div className="home-text">
+            <h1>Faycal Bezleghdir</h1>
+            <p>
+              Bonjour, je m'appelle Faycal Bezleghdir, je suis un développeur web
+              full stack. J'ai une passion pour le développement web et j'aime
+              créer des applications web.
+            </p>
+            <button className="btn">Voir mon travail</button>
+          </div>
+          <div className="home-img">
+            <img src="picture.jpg" alt="profile" />
+          </div>
+        </div>
+      </div>
+    );
+  }
+  
+  export default Home;
+  
---
+++ src/components/NavBar.js
@@ -0,0 +1,16 @@
+import "./NavBar.css";
+
+function NavBar() {
+  return (
+    <nav className="navbar">
+      <ul className="nav-links">
+        <li>Maison</li>
+        <li>CV</li>
+        <li>Projects</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default NavBar;
